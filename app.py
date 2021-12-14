fe515bd7ec5dceba8debe64ad1f6baf545a3ad97
Brian-FullStack <brian.fullstack@gmail.com>
2021-12-14 11:31:24 -0500

    Added render_template class, return index.html template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask #Importing Flask class
+from flask import Flask, render_template #Importing Flask class
 
 
 app = Flask(__name__) #Storing an instance of Flask in the app variable
@@ -7,7 +7,7 @@ app = Flask(__name__) #Storing an instance of Flask in the app variable
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 
 
